ebc6003d5550247eb3b2034f39449e19cbfd6a96
daniela-idara <purpleelephant1987@gmail.com>
2019-06-24 05:32:57 -0500

    todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+     text: '',
+     todos: [],
+     isClicked: false
+    };
+  }
+
+onClick = (event) => {
+  this.setState ({
+    isClicked: true,
+    todos: this.state.todos.concat(this.state.text),
+    text: ''
+  })
 }
+
+onChange = (event) => {
+  this.setState ({
+    text: event.target.value
+  });
+}
+
+render() {
+  return (
+        <div className="App">
+          {this.state.todos.map(function(value) {
+            return (
+              <p>{value}</p>
+            );
+          })}
+          
+          <input value={this.state.text} onChange={this.onChange}/>
+          <button onClick={this.onClick}>click me!</button>
+        </div>
+      );
+    }
+  }
 
 export default App;
